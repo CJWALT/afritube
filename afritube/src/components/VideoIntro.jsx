519d85a5dec36bd1c8a1aaef5a90de5222bb45a8
faithful <faithfulokondu@gmail.com>
2023-08-12 09:16:46 +0100

    vid border

--- afritube/src/components/VideoIntro.jsx
+++ afritube/src/components/VideoIntro.jsx
@@ -13,7 +13,7 @@ const VideoIntro = () => {
       <img src={cloud} className="absolute top-[-40px] left-0 z-[9] lt:top-[-80px]"/>
       <div className="container mx-auto px-[0px] py-6 xl:px-[200px]">
         <div className="relative flex flex-col justify-between items-center w-[100%] md:flex-row">
-          <span className="px-2">
+          <span className="p-2">
             <h1 className="font-bold text-3xl text-white relative z-[99]">Watch Fun Content</h1>
             <h2 className="font-medium text-xl py-2 relative z-[99] md:pr-[20%]">
               AfriTube is your ultimate destination for wholesome and
@@ -24,14 +24,16 @@ const VideoIntro = () => {
               <Button spec='relative z-[99] my-4 bg-black text-white hover:bg-[grey]' cta='Get Started'/>
             </NavLink>
           </span>
-          <div className="relative flex flex-col mt-5 ml-0 sm:ml-4 overflow-hidden sm:overflow-visible">
+          <div className="relative flex flex-col mt-5 rounded-lg overflow-hidden z-[99] sm:overflow-visible md:mr-[40px] md:rounded-none">
              <img src={sidecloud} className="w-[50px] absolute top-[-20px] left-[-30px]"/>
              <img src={sidecloud} className="w-[50px] absolute bottom-[0px] left-[-30px]" />
              <img src={sidecloud} className="w-[50px] absolute bottom-[0px] right-[-20px]" />
              <img src={sidecloud} className="w-[50px] absolute top-[-15px] right-[-10px]" />
-            <video controls  className="bg-white p-4 w-[400px] h-[250px] object-cover rounded-lg p-[20px] relative z-[99]">
+             <span className="w-[100%] h-[250px] bg-white p-[20px] relative z-[99] md:w-[350px]">
+             <video controls  className="h-[100%] w-[100%]  object-cover rounded-[10px]">
               <source src={vid} type="video/mp4"/>
             </video>
+             </span>
           </div>
         </div>
       </div>
