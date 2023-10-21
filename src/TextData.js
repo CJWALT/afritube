d6654d503e8cdfdfa3aa2751791b0ed577667850
faithful <faithfulokondu@gmail.com>
2023-10-21 14:21:08 +0100

    update

---
+++ src/TextData.js
@@ -0,0 +1,40 @@
+import toons from '../src/assets/TextThumbnail/books.webp';
+import books from '../src/assets/TextThumbnail/books.webp'
+import facts from '../src/assets/TextThumbnail/facts.webp';
+import kingdoms from '../src/assets/TextThumbnail/kingdoms.webp';
+import foods from '../src/assets/TextThumbnail/popularfoods.webp';
+import dances from '../src/assets/TextThumbnail/dances.webp'
+
+
+export const literature =  [
+    {
+        "id" : 1,
+        "name" : "African Toons",
+        "thumbnail" : toons
+    },
+    {
+        "id" : 2,
+        "name" : "African Books",
+        "thumbnail" : books
+    },
+    {
+        "id" : 3,
+        "name" : "African Facts",
+        "thumbnail" : facts
+    },
+    {
+        "id" : 4,
+        "name" : "African Kingdoms",
+        "thumbnail" : kingdoms
+    },
+    {
+        "id" : 5,
+        "name" : "Popular Foods",
+        "thumbnail" : foods
+    },
+    {
+        "id" : 6,
+        "name" : "Famout African Dances",
+        "thumbnail" : dances
+    }
+]
